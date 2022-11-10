164b3ca6525fd8480774fd061269c3991c049e26
ImVicente <101204565+ImVicente@users.noreply.github.com>
2022-11-10 15:57:49 -0300

    local

--- app2/src/components/producto.js
+++ app2/src/components/producto.js
@@ -1,25 +1,32 @@
-import Button from 'react-bootstrap/Button';
 
-const Producto = ({data, addToCart}) => {
+const Producto = ({data, addToCart, delFromCart}) => {
     
-    let {name,precio,imagen,id} = data;
+    let {name,precio,imagen,id,cantidad} = data;
   
     return (
     
     <div style={{border:"thin solid gray", padding:"1rem"}}>
-      <div>
+      <div className="fotos">
         <h4>{name}</h4>
+      </div>
+      <div >
         <h5>${precio} CLP</h5>
       </div>
       <div>
         <img
-            className="d-block w-100"
+            className="d-block w-100 fotos2"
             src={imagen}
           />
       </div>
-        <br></br>
-        <Button style={{backgroundColor: 'indigo', borderColor:'indigo'}} onClick={() => addToCart(id)}>Agregar</Button>
- 
+        <br/>
+        <button  type="button" class="btn btn-success" onClick={() => addToCart(id)}>Agregar</button>
+        <br/>
+        
+        <div class="alert alert-success" role="alert">
+          This is a success alert—check it out!
+        </div>
+        <button type="button" class="btn btn-danger" onClick={()=>delFromCart(id)}>Eliminar Uno</button>
+        <br/> 
     </div>
   )
 }
