2d5d1d60bb381e437d566c3522d9917570f2a682
ImVicente <101204565+ImVicente@users.noreply.github.com>
2022-11-10 15:55:58 -0300

    local

--- app2/src/reducers/carritoReducer.js
+++ app2/src/reducers/carritoReducer.js
@@ -2,6 +2,7 @@ import { TYPES } from "../actions/carritoAction";
 
 export const carritoEstadoInicial = {
     products : [],
+    
     cart : []
 }
 
@@ -11,10 +12,18 @@ export function carritoReducer(state , action){
         let newItem = state.products.find((product) => product.id === action.payload);
         
         let itemInCart = state.cart.find(item=>item.id===newItem.id)
+        let car_sin_producto=[...state.cart,{...newItem,cantidad:1}]
         
+        let car_con_producto=[...state.cart.map((item)=>item.id===newItem.id?{...item,cantidad:item.cantidad+1}:item)]
+
+        if (!itemInCart){localStorage.setItem("carro", JSON.stringify(car_sin_producto))}else{
+        localStorage.setItem("carro", JSON.stringify(car_con_producto))}
+
+
         return itemInCart
         ?{...state, cart: state.cart.map((item)=>item.id===newItem.id?{...item,cantidad:item.cantidad+1}:item)}
         :{...state, cart:[...state.cart,{...newItem, cantidad:1}]}
+        
 
         }
 
@@ -22,11 +31,41 @@ export function carritoReducer(state , action){
 
         case TYPES.REMOVE_ONE_FROM_CART:{
         let itemToDelete = state.cart.find((item)=>item.id===action.payload);
+        var carro = JSON.parse(localStorage.getItem("carro"))
+
+        
+
+
+        
+
+        if(itemToDelete.cantidad === 1){
+            let newData = carro.filter((item) => item.id !== action.payload)
+            localStorage.setItem("carro", JSON.stringify(newData));
+        }else{
+            let producto = carro.find((product) => product.id === action.payload);
+        
+            producto.cantidad= itemToDelete.cantidad - 1 ;
+            localStorage.setItem("carro", JSON.stringify(carro));}
+            
+ 
+        
+            
+
+
         return itemToDelete.cantidad > 1 
         ?{...state, cart: state.cart.map((item) => item.id===action.payload?{...item,cantidad:item.cantidad-1}:item)}
         :{...state, cart: state.cart.filter((item) => item.id !== action.payload),}
         };
+
+
+
         case TYPES.REMOVE_ALL_FROM_CART:{
+            
+            var carro = JSON.parse(localStorage.getItem("carro"))
+            let newData = carro.filter((item) => item.id !== action.payload)
+            localStorage.setItem("carro", JSON.stringify(newData));
+
+
         return {...state, cart: state.cart.filter((item) => item.id !== action.payload),}
         }
         
@@ -37,14 +76,22 @@ export function carritoReducer(state , action){
                 
             };
             }
-        case TYPES.GUARDAR_CARRO: { 
-                let car=state.cart
-                localStorage.setItem("compras", JSON.stringify(car)) 
-
-                
+        
+        case TYPES.LOCAL: { 
+            
+            return{
+                ...state,
+                cart : action.payload.map(data=>data),
             }
+
+            
+        }
 
         default:
             return state;
     }
 }
+
+
+
+
